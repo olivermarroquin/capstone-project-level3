922294b426d13ab97d116d3e3c62e8d4ac8b38ed
omarroq2 <oliver.marroquin31217@gmail.com>
2026-05-29 13:44:46 -0400

    Add vehicle details page

--- src/pages/VehicleDetails.jsx
+++ src/pages/VehicleDetails.jsx
@@ -1,9 +1,46 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import VehicleService from "../services/VehicleService";
 
 export default function VehicleDetails() {
+  const { id } = useParams();
+  const [vehicle, setVehicle] = useState(null);
+
+  useEffect(() => {
+    async function getVehicle() {
+      const { data, error } = await VehicleService.getVehicleById(id);
+
+      console.log("Single vehicle:", data);
+
+      if (error) {
+        console.log(error);
+        return;
+      }
+      setVehicle(data);
+    }
+    getVehicle();
+  }, [id]);
+
+  if (!vehicle) {
+    return <p>Loading vehicle...</p>;
+  }
+
   return (
-    <div>
-      <h2>Vehicle Details Page</h2>
-    </div>
+    <section>
+      <h2>
+        {vehicle.year} {vehicle.make} {vehicle.model}
+      </h2>
+
+      <img
+        src={vehicle.image_url}
+        alt={`${vehicle.year} ${vehicle.make}`}
+        width="400"
+      />
+
+      <p>Price: ${vehicle.price}</p>
+      <p>Mileage: ${vehicle.mileage}</p>
+      <p>Statis: ${vehicle.status}</p>
+      <p>{vehicle.description}</p>
+    </section>
   );
 }
